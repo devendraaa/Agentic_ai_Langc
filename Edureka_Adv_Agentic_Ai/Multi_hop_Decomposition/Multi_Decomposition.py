29709c579c0e21eefc72936c9d638f49c13cd0ed
devendraaa <devendrachavan31@gmail.com>
2026-07-30 13:53:05 +0530

    new code updated

--- Edureka_Adv_Agentic_Ai/Multi_hop_Decomposition/Multi_Decomposition.py
+++ Edureka_Adv_Agentic_Ai/Multi_hop_Decomposition/Multi_Decomposition.py
@@ -12,6 +12,7 @@ from langchain_chroma import Chroma
 from typing import Literal
 from pydantic import BaseModel, Field
 from sentence_transformers import CrossEncoder
+from collections import defaultdict
 
 load_dotenv()
 
@@ -70,7 +71,9 @@ class GraphState(TypedDict, total=False):
     user_query : str
     retrival_name : list[str]
     sub_queries : list[str]
-    all_document : list[Document]
+    hop_result : list[dict]
+    fused_result: list[dict]
+    all_evidence: list[dict]
     select_ret : list[str]
 
 class Decomposition_query(BaseModel):
@@ -126,8 +129,8 @@ router_llm = llm.with_structured_output(RouterRespones)
 router_prompt = """
     Available retrievers:
 
-    - docker_vs
-    - ai_agent_vs
+    - docker
+    - ai_agent
     - aws
     
     User query:
@@ -139,7 +142,7 @@ router_prompt = """
 
 def Route_Query(state : GraphState):
 
-    hop_result = []
+    hop_res = []
 
     for hop, que in enumerate(state["sub_queries"], start=1):
         r_prompt = router_prompt.format(
@@ -151,21 +154,201 @@ def Route_Query(state : GraphState):
 
         re = select_re.select_ret
 
+        retts = []
+
         for i in re:
 
             ret = retrival[i]
 
             result = ret.invoke(que)
 
-            hop_result.append({
-                "hop": hop,
-                "query": que,
-                "selected_retrieval": re,
-                "retriever": i,
+            retts.append({
+                "retriever" : i,
                 "docs" : result
             })
+
+        hop_res.append({
+                "hop": hop,
+                "query": que,
+                "retrievers": retts
+            })
+        
+    state["hop_result"] = hop_res
             
-    return hop_result
+    return state
+
+
+
+# state = decompose_query(state)
+
+# all_retrival = Route_Query(state)
+
+def rrf_fusion(state: GraphState, k=60):
+
+    fused_results = []
+
+    for hop in state["hop_results"]:
+
+        retrievals = hop["retrievers"]
+
+        # If only one retriever, no fusion needed
+        if len(retrievals) == 1:
+
+            docs = retrievals[0]["docs"]
+
+            hop["rrf_docs"] = [
+                {
+                    "document": doc,
+                    "score": None
+                }
+                for doc in docs
+            ]
+
+            fused_results.append(hop)
+            continue
+
+        rrf_scores = defaultdict(float)
+        document_lookup = {}
+
+        # Calculate RRF scores
+        for retrieval in retrievals:
+
+            docs = retrieval["docs"]
+
+            for rank, doc in enumerate(docs, start=1):
+
+                # Unique document identifier
+                doc_id = (
+                    doc.metadata.get("source", "")
+                    + "_"
+                    + str(doc.metadata.get("page", ""))
+                    + "_"
+                    + str(rank)
+                )
+
+                rrf_scores[doc_id] += 1 / (k + rank)
+
+                document_lookup[doc_id] = doc
+
+        # Sort by RRF score
+        ranked_docs = sorted(
+            rrf_scores.items(),
+            key=lambda x: x[1],
+            reverse=True
+        )
+
+        fused_docs = []
+
+        for doc_id, score in ranked_docs:
+
+            fused_docs.append(
+                {
+                    "document": document_lookup[doc_id],
+                    "score": score,
+                    "source": document_lookup[doc_id].metadata.get("source"),
+                    "page": document_lookup[doc_id].metadata.get("page")
+                }
+            )
+
+        hop["rrf_docs"] = fused_docs
+
+        fused_results.append(hop)
+
+    state["fused_result"] = fused_results
+
+    return state
+
+# f_result = rrf_fusion(all_retrival)
+
+def print_rrf_results(rrf_results):
+
+    for hop in rrf_results:
+
+        print("=" * 100)
+        print(f"Hop : {hop['hop']}")
+        print(f"Query : {hop['query']}")
+
+        print("\nRRF Ranking:\n")
+
+        for rank, item in enumerate(hop["rrf_docs"], start=1):
+
+            doc = item["document"]
+            score = item["score"]
+
+            score_text = "N/A" if score is None else f"{score:.6f}"
+
+            print(f"[{rank}] Score : {score_text}")
+            print(f"Source : {doc.metadata.get('source')}")
+            print(f"Page   : {doc.metadata.get('page')}")
+            print(doc.page_content[:200].replace("\n", " "))
+            print("-" * 100)
+
+def aggregate_evidence(state: GraphState):
+
+    all_eviden = []
+
+    for hop in state["fused_result"]:
+        for item in hop["rrf_docs"]:
+            all_eviden.append(
+            {
+                "hop": hop["hop"],
+                "query": hop["query"],
+                "document": item["document"],
+                "score": item["score"]
+            }
+        )
+
+    state["all_evidence"] = all_eviden
+
+    return state
+    # return {
+    #     "hop_results": rrf_results,
+    #     "all_evidence": all_evidence
+    # }
+
+# aggre_result = aggregate_evidence(f_result)
+
+def show_retrieval_results(state: GraphState):
+
+    for retrieval in state["all_evidence"]:
+        print("=" * 100)
+        print(f"Retrieval #{retrieval['hop']}")
+        print(f"Query     : {retrieval['query']}")
+        # print(f"All Retrieval : {retrieval['retriever']}")
+        # print(f"Selected Retriever : {retrieval['selected_retrieval']}")
+        print(f"Documents : {len(retrieval['document'])}")
+        print(f"Score : {retrieval['score']}")
+
+        for i, doc in enumerate(retrieval["document"], start=1):
+            print(f"\n[{i}] {doc.metadata.get('book')} | Page {doc.metadata.get('page')}")
+            print(doc.page_content[:150].replace("\n", " ") + "...")
+
+
+workflow = StateGraph(GraphState)
+
+workflow.add_node("decompose_query", decompose_query)
+
+workflow.add_node("Route_Query", Route_Query)
+
+workflow.add_node("rrf_fusion", rrf_fusion)
+
+workflow.add_node("aggregate_evidence", aggregate_evidence)
+
+workflow.add_node("show_retrieval_results", show_retrieval_results)
+
+workflow.add_edge(START, "decompose_query")
+
+workflow.add_edge("decompose_query", "Route_Query")
+
+workflow.add_edge("Route_Query", "rrf_fusion")
+
+workflow.add_edge("rrf_fusion", "aggregate_evidence")
+
+workflow.add_edge("aggregate_evidence", "show_retrieval_results")
+
+workflow.add_edge("show_retrieval_results", END)
+
+graph = workflow.compile()
 
 state = {
     "original_query" : input("/n enter user query")
@@ -173,21 +356,14 @@ state = {
 
 state["user_query"] = state["original_query"]
 
-state = decompose_query(state)
+from IPython.display import Image, display
 
-all_retrival = Route_Query(state)
+try:
+    png = graph.get_graph().draw_mermaid_png()
+    with open("Multi_decomposition_graph.png", "wb") as f:
+        f.write(png)
+    print("graph save as graph.png")
+except Exception as e:
+    print("Error:",e)
 
-def show_retrieval_results(results):
-    for retrieval in results:
-        print("=" * 100)
-        print(f"Retrieval #{retrieval['hop']}")
-        print(f"Query     : {retrieval['query']}")
-        print(f"All Retrieval : {retrieval['retriever']}")
-        print(f"Selected Retriever : {retrieval['selected_retrieval']}")
-        print(f"Documents : {len(retrieval['docs'])}")
 
-        for i, doc in enumerate(retrieval["docs"], start=1):
-            print(f"\n[{i}] {doc.metadata.get('book')} | Page {doc.metadata.get('page')}")
-            print(doc.page_content[:150].replace("\n", " ") + "...")
-
-show_retrieval_results(all_retrival)
